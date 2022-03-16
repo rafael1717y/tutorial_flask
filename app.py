3671f6ae63c69081b5a1f2e13cdcc0d2778ec47a
rafael1717y <rafael1717yy@gmail.com>
2022-03-16 09:10:24 -0300

    aula3

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+
+
 
 # O decorator aplicando o método 'route' passando ('/) em cima da função 'index'.
 # app.route -> cria a rota da página
@@ -15,12 +21,7 @@ if __name__ == 'main':
 
 
 """
-# Aula 2 - Resumo para criação do ambiente
+# Aula 3 - Resumo para criação do ambiente
 >> -----------------------------------<<
-01. python -m venv env
-02. pip install virtualenv
-03. virtualenv env
-04. source env/Scripts/activate
-05. set FLASK_APP = app.py
-06. flask run
+01. 
 """
